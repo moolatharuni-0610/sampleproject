f1969fae493e1ccd9b8f997a71652396d6eafba3
moolatharuni-0610 <moolatharuni@gmail.com>
2025-08-02 11:44:53 +0530

    first update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
-
 @app.route("/")
 def index():
     return "Welcome to Mrecw"
+@app.route("/home")
+def home():
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
